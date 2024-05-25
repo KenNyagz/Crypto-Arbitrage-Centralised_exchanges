84243f164b848612525ebee90242bb2666f1efa8
KenNyagz <kennyagz681@gmail.com>
2024-05-25 23:09:49 +0300

    Realised my arbitrage logic is majorly flawed, nedd to rethink it

--- arbtg.py
+++ arbtg.py
@@ -28,26 +28,42 @@ def get_arbtg(exchange1, exchange2, exchange1_tickers, exchange2_tickers):
         if i in exchange2_tickers:
             common_tickers.update({i: exchange1_tickers[i]})
 
-    print(f"\nCommon tokens in {exchange1} and {exchange2} are", len(common_tickers), "\n")
+    print(f"\nCommon tickers in {exchange1} and {exchange2} are", len(common_tickers), "\n")
+
     percentage_diffs = {f"percentage_to_{exchange1}": 0.0, f"percentage_to_{exchange1}": 0.0}
     for i in common_tickers:
         if exchange1_tickers[i] > exchange2_tickers[i]:
             difference = exchange1_tickers[i] - exchange2_tickers[i]
-            percentage_diffs[f"percentage_to_{exchange1}"] = difference / exchange2_tickers[i] * 100
-            percentage_diffs[f"percentage_to_{exchange2}"] = (difference / exchange1_tickers[i]) * 100
+            print(exchange1_tickers[i], exchange2_tickers[i], difference)
+            break
+            difference = exchange1_tickers[i] - exchange2_tickers[i]
+
+            try:
+                percentage_diffs[f"percentage_to_{exchange1}"] = difference / exchange2_tickers[i] * 100
+                percentage_diffs[f"percentage_to_{exchange2}"] = (difference / exchange1_tickers[i]) * 100
+            except ZeroDivisionError as e:
+                 print(f"ERROR: {exchange1_tickers[i]} - {exchange2_tickers[i]}")
+
             if percentage_diffs[f"percentage_to_{exchange1}"] > 3:
                 print(i, end=": ")
                 print("difference is", percentage_diffs[f"percentage_to_{exchange1}"], f"% favouring {exchange1}")
+
         elif exchange2_tickers[i] > exchange1_tickers[i]:
             difference = exchange2_tickers[i] - exchange2_tickers[i]
-            percentage_diffs[f"percentage_to_{exchange2}"] = difference / exchange2_tickers[i] * 100
-            percentage_diffs[f"percentageto_{exchange1}"] = (difference / exchange1_tickers[i]) * 100
+            try:
+                percentage_diffs[f"percentage_to_{exchange2}"] = difference / exchange2_tickers[i] * 100
+                percentage_diffs[f"percentageto_{exchange1}"] = (difference / exchange1_tickers[i]) * 100
+            except ZeroDivisionError as e:
+                 print(f"ERROR: price1:{exchange1_tickers[i]} - price2:{exchange2_tickers[i]}")
+
             if percentage_diffs[f"percentage_to_{exchange2}"] > 3:
                 print(i, end=": ")
+                print(f" {exchange1_tickers[i]} <-> {exchange2}")
                 print("difference is", percentage_diffs[f"percentage_to_{exchange2}"], f"% favouring {exchange2}")
 
 
 #print('\n OkX and Binance\n', get_arbtg('okx', 'binance', okx_tickers, binance_tickers))
-#print('\n Huobi and Binance\n', get_arbtg('huobi', 'binance', huobi_tickers, binance_tickers))
+print('\n Huobi and Binance\n', get_arbtg('huobi', 'binance', huobi_tickers, binance_tickers))
 #print('\n OkX and Huobi\n', get_arbtg('okx', 'huobi', okx_tickers, huobi_tickers))
-get_arbtg('binance', 'gateio', binance_tickers, gateio_tickers)
+#get_arbtg('binance', 'gateio', binance_tickers, gateio_tickers)
+
