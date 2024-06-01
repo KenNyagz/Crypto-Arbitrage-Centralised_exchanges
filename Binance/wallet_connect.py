f7b9f6c0c4bc792e005e8057ae0232528c73f189
KenNyagz <kennyagz681@gmail.com>
2024-06-01 23:10:22 +0300

    withdrawal functionality added

--- Binance/wallet_connect.py
+++ Binance/wallet_connect.py
@@ -1,17 +1,50 @@
 from binance.client import Client
+import time
 
-with open('api_secret', 'r') as f: 
-        exchange_secret = f.read()
-        exchange_secret = exchange_secret.strip()
-with open('api_key', 'r') as f:
-	exchange_key = f.read()
-	exchange_key = exchange_key.strip() # Clean new line character
 
-client = Client(exchange_key, exchange_secret)
+def get_keys(file_api_secret, file_api_key):
+	if type(file_api_key) != str or type(file_api_secret) != str:
+		raise TypeError("Filenames must be strings")
 
-account_info = client.get_account()
-balances = account_info['balances']
-for balance in balances:
-	if float(balance['free']) > 0:
- 		print(f"{balance['asset']} : {balance['free']}")
-#print(balances)
+	with open(file_api_secret, 'r') as f: 
+        	exchange_secret = f.read()
+	        exchange_secret = exchange_secret.strip()# Clean new line character
+
+	with open(file_api_key, 'r') as f:
+		exchange_key = f.read()
+		exchange_key = exchange_key.strip() # Clean new line character
+
+	return [exchange_secret, exchange_key]
+
+
+def wallet_connect(exchange_key, exchange_secret):
+	client = Client(exchange_key, exchange_secret)
+
+	withdrawal = client.withdraw(
+ 				asset='BNB',
+				address='', # address to
+				amount='0.001',
+				network='BEP20'
+				)
+	time.sleep(30) # Sleep for a short time to ensure tx goes
+
+	account_info = client.get_account()
+	balances = account_info['balances']
+	bals = []
+	for balance in balances: # Fetch available balances
+		if float(balance['free']) > 0:
+			bals.append(balance)
+ 			#print(f"{balance['asset']} : {balance['free']}")
+
+	withdrawal = client.withdraw(
+				asset='BNB',
+				address='',
+				amount='0.001',
+				network='BEP20')
+	
+	return bals
+	
+
+[api_secret, api_key] = get_keys('api_secret', 'api_key')
+balances = wallet_connect(api_key, api_secret)
+print(balances)
