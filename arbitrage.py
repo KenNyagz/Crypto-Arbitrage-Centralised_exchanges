c2c6383477d05b02652611e89c0dd1ec9f9a3e4c
KenNyagz <kennyagz681@gmail.com>
2024-06-13 16:50:08 +0300

    run all exchanges

--- arbitrage.py
+++ arbitrage.py
@@ -69,7 +69,7 @@ def get_arbtg(exchange1, exchange2, exchange1_tickers, exchange2_tickers):
 
     return result
 
-#print('\n OkX and Binance\n', get_arbtg('okx', 'binance', okx_tickers, binance_tickers))
-#print('\n Huobi and Binance\n', get_arbtg('huobi', 'binance', huobi_tickers, binance_tickers))
-#print('OkX and Huobi\n', get_arbtg('okx', 'huobi', okx_tickers, huobi_tickers))
+print('\n OkX and Binance\n', get_arbtg('okx', 'binance', okx_tickers, binance_tickers))
+print('\n Huobi and Binance\n', get_arbtg('huobi', 'binance', huobi_tickers, binance_tickers))
+print('OkX and Huobi\n', get_arbtg('okx', 'huobi', okx_tickers, huobi_tickers))
 print('\n binance and gateio\n',get_arbtg('binance', 'gateio', binance_tickers, gateio_tickers))
