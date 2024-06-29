95315704602f91ac7bfefd9a29d5ee88aa67bd63
KenNyagz <kennyagz681@gmail.com>
2024-06-29 23:04:26 +0300

    bitget tickers

---
+++ Bitget/get_tickers.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def get_tickers(okx_secret=None):
+    url = 'https://api.bitget.com/api/spot/v1/market/tickers'
+    response = requests.get(url)
+    data = response.json()['data']
+
+    pairs_prices = {}
+    for i in data:
+        pair = i['symbol']
+        price = i['close']
+        pairs_prices.update({pair: float(price)})
+
+    #for pair in pairs_prices:
+    #    print(pair, ':', pairs_prices[pair]) # visualisation
+    return pairs_prices
+
+
+if __name__ == "__main__":
+    get_tickers()
