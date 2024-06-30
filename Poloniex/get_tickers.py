aeea46c9e0ce2995a9c850631f07c51106ed2805
KenNyagz <kennyagz681@gmail.com>
2024-06-30 17:50:38 +0300

    Poloniex pairs

---
+++ Poloniex/get_tickers.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+
+def get_tickers():
+    url = "https://api.poloniex.com/markets/price"
+
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        data = resp.json()
+    else:
+        print("Error")
+
+    pairs_prices = {}
+    for i in data:
+        for key in i:
+            pair = i['symbol']
+            price = i['price']
+            pairs_prices.update({pair.upper(): price}) #Uppercase for uniformity
+
+    #for pair in pairs_prices:
+    #    print(pair + " : " + str(pairs_prices[pair]))  # len~990
+
+    return pairs_prices
+
+if __name__ == "__main__":
+    get_tickers()
