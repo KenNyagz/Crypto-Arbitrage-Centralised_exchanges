82fe5b0f210a467388c0e614c11801b72a869428
KenNyagz <kennyagz681@gmail.com>
2024-06-30 23:17:14 +0300

    back-up or best running code so far

--- test-tube.py
+++ test-tube.py
@@ -1,41 +1,10 @@
-
-with open("Binance/api_secret", 'r') as f:
-    binance_secret = f.read()
-with open("OKX/api_secret", 'r') as f:
-    okx_secret = f.read()
-
-
-## Huobi x Binance
-common_tickers_BxH = {}
-for i in huobi_tickers:
-    if i in binance_tickers:
-        common_tickers_BxH.update({i: huobi_tickers[i]})
-
-print("\nCommon tokens in binance and huobi are", len(common_tickers_BxH), "\n")
-
-for i in common_tickers_BxH:
-    if huobi_tickers[i] > binance_tickers[i]:
-        difference = huobi_tickers[i] - binance_tickers[i]
-        percentage_to_binance = difference / binance_tickers[i] * 100
-        percentage_to_huobi = (difference / huobi_tickers[i]) * 100
-        if percentage_to_huobi > 3:
-            print(i, end=": ")
-#            print("difference is", percentage_to_huobi, "% favouring huobi")
-    elif binance_tickers[i] > huobi_tickers[i]:
-        difference = binance_tickers[i] - huobi_tickers[i]
-        percentage_to_binance = difference / binance_tickers[i] * 100
-        percentage_to_huobi = (difference / huobi_tickers[i]) * 100
-        if percentage_to_binance > 3:
-            print(i, end=": ")
-#            print("difference is", percentage_to_binance, "% favouring binance")
-
-'=========================================================================='
-
-from binance import Client
+#!/usr/bin/python3
 from Binance.get_tickers import get_tickers as binance_tickers_
 from Huobi.get_tickers import get_tickers as huobi_tickers_
 from OKX.get_tickers import get_tickers as okx_tickers
 from GateIO.get_tickers import get_tickers as gateio_tickers
+from Bybit.get_tickers import get_tickers as bybit_tickers
+#from binance import Client
 
 def get_secret(file_name):
     if not isinstance(file_name, str):
@@ -53,6 +22,7 @@ binance_tickers = binance_tickers_(binance_secret)
 huobi_tickers = huobi_tickers_() #
 okx_tickers = okx_tickers(okx_secret)
 gateio_tickers = gateio_tickers("test") #
+bybit_tickers = bybit_tickers('tst')
 
 
 def get_arbtg(exchange1, exchange2, exchange1_tickers, exchange2_tickers):
@@ -62,7 +32,7 @@ def get_arbtg(exchange1, exchange2, exchange1_tickers, exchange2_tickers):
             common_tickers[ticker] = exchange1_tickers[ticker]
 
     result = f"\nCommon tickers in {exchange1} and {exchange2} are: {len(common_tickers)}\n"
-    print(result)
+    #print(result)
 
     percentage_diffs = {f"percentage_to_{exchange1}": 0.0, f"percentage_to_{exchange2}": 0.0}
     for ticker in common_tickers:
@@ -73,13 +43,15 @@ def get_arbtg(exchange1, exchange2, exchange1_tickers, exchange2_tickers):
                 percentage_diffs[f"percentage_to_{exchange1}"] = difference / exchange1_tickers[ticker] * 100
                 percentage_diffs[f"percentage_to_{exchange2}"] = (difference / exchange2_tickers[ticker]) * 100
             except ZeroDivisionError as e:
-                 print(f"ERROR: {exchange1_tickers[ticker]} - {exchange2_tickers[ticker]}")
+                 #print(f"zeroDivERROR:{ticker} : {exchange1_tickers[ticker]} ~ {exchange2_tickers[ticker]}")
                  continue
 
             if percentage_diffs[f"percentage_to_{exchange1}"] > 3:
-                print(ticker, end=": ")
-                print(percentage_diffs[f"percentage_to_{exchange1}"], f"% favouring {exchange1}")
-                #result += f"{ticker}: difference is {percentage_diffs[f'percentage_to_{exchange1}']}% favouring {exchange1}\n" # for contructing return value
+                #print(ticker, end=" : ")
+                # #print(percentage_diffs[f"percentage_to_{exchange1}"], f"% favouring {exchange2}") ## Don't uncomment unless testing
+                #print(percentage_diffs[f"percentage_to_{exchange2}"], f"% favouring {exchange1}")
+                # #result += f"{ticker} : difference is {percentage_diffs[f'percentage_to_{exchange1}']}% favouring {exchange2}\n" ## Don't uncomment unless testing
+                result += f"{ticker} : difference is {percentage_diffs[f'percentage_to_{exchange2}']}% favouring {exchange1}\n" # return value construct
 
         elif exchange2_tickers[ticker] > exchange1_tickers[ticker]:
             difference = exchange2_tickers[ticker] - exchange1_tickers[ticker]
@@ -87,17 +59,25 @@ def get_arbtg(exchange1, exchange2, exchange1_tickers, exchange2_tickers):
                 percentage_diffs[f"percentage_to_{exchange2}"] = difference / exchange2_tickers[ticker] * 100
                 percentage_diffs[f"percentage_to_{exchange1}"] = (difference / exchange1_tickers[ticker]) * 100
             except ZeroDivisionError as e:
-                 print(f"ERROR: price1:{exchange1_tickers[ticker]} - price2:{exchange2_tickers[ticker]}")
+                 #print(f"zeroDivERROR:{ticker} : {exchange1_tickers[ticker]} ~ {exchange2_tickers[ticker]}")
                  continue
 
             if percentage_diffs[f"percentage_to_{exchange2}"] > 3:
-                print(ticker, end=": ")
-                print(percentage_diffs[f"percentage_to_{exchange2}"], f"% favouring {exchange2}")
-                #result += f"{ticker}: difference is {percentage_diffs[f'percentage_to_{exchange1}']}% favouring {exchange1}\n" #Constructing return value
+                #print(ticker, end=" : ")
+                # #print(percentage_diffs[f"percentage_to_{exchange2}"], f"% favouring {exchange1}") ## Don't uncomment unless testing
+                #print(percentage_diffs[f"percentage_to_{exchange1}"], f"% favouring {exchange2}")
+                # #result += f"{ticker} : difference is {percentage_diffs[f'percentage_to_{exchange2}']}% favouring {exchange1}\n" ## Don't uncomment unless testing
+                result += f"{ticker} : difference is {percentage_diffs[f'percentage_to_{exchange1}']}% favouring {exchange2}\n" #Constructing return value
 
-    #return result
+    return result
 
-#print('\n OkX and Binance\n', get_arbtg('okx', 'binance', okx_tickers, binance_tickers))
-#print('\n Huobi and Binance\n', get_arbtg('huobi', 'binance', huobi_tickers, binance_tickers))
-print('\n OkX and Huobi\n', get_arbtg('okx', 'huobi', okx_tickers, huobi_tickers))
-#print('\n binance and gateio\n',get_arbtg('binance', 'gateio', binance_tickers, gateio_tickers))
+print('\n Binance and Bybit\n',get_arbtg('binance', 'bybit', binance_tickers, bybit_tickers))
+print('\n Binance and Huobi\n', get_arbtg('binance', 'huobi', binance_tickers, huobi_tickers))
+print('\n Binance and OKX\n', get_arbtg('binance', 'okx', binance_tickers, okx_tickers))
+print('\n Binance and gateio\n',get_arbtg('binance', 'gateio', binance_tickers, gateio_tickers))
+print('\n Bybit and Huobi\n', get_arbtg('bybit', 'huobi', bybit_tickers, huobi_tickers))
+print('\n Bybit and OKX\n', get_arbtg('bybit', 'okx', bybit_tickers, okx_tickers,))
+print('\n Bybit and Gateio\n',get_arbtg('bybit', 'gateio', bybit_tickers, gateio_tickers))
+print('\n Huobi and OKX\n', get_arbtg('huobi', 'okx', huobi_tickers, okx_tickers))
+print('\n Huobi and GateIO\n', get_arbtg('huobi', 'gateio', huobi_tickers, gateio_tickers))
+print('\n OKX and GateIO\n', get_arbtg('okx', 'gateio', okx_tickers, gateio_tickers))
